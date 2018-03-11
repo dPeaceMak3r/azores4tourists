e82f06316d7aec94ddbea856f8ce7394579d2a4f
Leandro Jesus <make.dpeace2@gmail.com>
2018-03-11 20:25:40 -0100

    Added nav component

--- imports/ui/components/nav/nav.js
+++ imports/ui/components/nav/nav.js
@@ -1,1 +1,19 @@
 import './nav.html';
+import '../../stylesheets/home.less';
+
+Template.nav.onCreated(function () {
+  console.log("started");
+});
+
+Template.nav.helpers({
+  sayHello() {
+    console.log("hello");
+  },
+});
+
+Template.nav.events({
+  'submit .info-link-add'(event) {
+    event.preventDefault();
+
+  },
+});
