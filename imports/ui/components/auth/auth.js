3b54d74236114a9446ae21a8b9da660fc14b17ac
Dário Ávila <darionsavila@gmail.com>
2018-03-12 00:21:43 -0100

    fb picture added

--- imports/ui/components/auth/auth.js
+++ imports/ui/components/auth/auth.js
@@ -1,1 +1,16 @@
 import './auth.html';
+c
+//"http://graph.facebook.com/\" + user.services.facebook.id + \"/picture/?type=medium"
+//"http://graph.facebook.com/100001524268375/picture/?type=small"
+
+Template.Auth.helpers({
+    image: function () {
+        if (!Meteor.user()) {
+            return null;
+        }
+        console.log(Meteor.user());
+        return Meteor.user().profile.picture;
+
+    }
+
+});
--- server/main.js
+++ server/main.js
@@ -2,3 +2,16 @@
 
 import '/imports/startup/server';
 import '/imports/startup/both';
+
+
+Meteor.startup(function(){
+    Accounts.onCreateUser(function(options, user){
+        if (options.profile){
+            user.profile = options.profile;
+        }
+
+        user.profile.picture =
+            `http://graph.facebook.com/${user.services.facebook.id}/picture/?type=small`;
+        return user;
+    })
+});
